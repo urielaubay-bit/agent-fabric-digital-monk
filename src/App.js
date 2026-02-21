179234e3e556b26b7957d7c6283e0fdac625869a
Jose <jose@agent-fabric.ai>
2026-02-21 05:25:12 +0000

    feat: implement Hero Section [HERO-001]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      {/* Add other components here */}
+    </div>
+  );
+}
+
+export default App;
